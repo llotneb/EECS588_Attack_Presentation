2fa51a9db95eb93a3f450bf51d7a2431b8df8936
Ben Toll <tollben@umich.edu>
2016-04-05 00:41:14 -0400

    Added iptables cmd for Eve

--- start_eve.sh
+++ start_eve.sh
@@ -1,4 +1,10 @@
 # This script starts Eve, the packet detector
+
+# Configure the UNIX Kernel to queue all input packets from $HOST
+# on queue 0.
+HOST="127.0.0.1"
+sudo iptables -A INPUT -s $HOST -j NFQUEUE --queue-num 0
+
 if [ ! -e "inject" ]
 then
     make
