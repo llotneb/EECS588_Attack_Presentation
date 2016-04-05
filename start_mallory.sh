31c405084bcbc44f45785235b8053004fd0518b6
Ubuntu <ubuntu@ip-10-0-0-51.ec2.internal>
2016-04-05 04:22:23 +0000

    add sudo

--- start_mallory.sh
+++ start_mallory.sh
@@ -2,4 +2,4 @@
 #     Queue 0, inject, two client connections, period of 1(s).
 #     Slow speed is 10, fast speed is 150.
 #     Look for the username Satoshi in log files.
-./inject 0 inject 2 1 10 150 Satoshi
+sudo ./inject 0 inject 2 1 10 150 Satoshi
